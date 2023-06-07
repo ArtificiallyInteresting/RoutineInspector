9b4d3ad539be4323400a3401ea889f2b0dd360e6
Henry Smith <artificiallyinteresting@gmail.com>
2023-06-07 10:39:50 -0700

    Testing streamlit form

--- app.py
+++ app.py
@@ -1,4 +1,28 @@
 import streamlit as st
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
+model_name = "deepset/roberta-base-squad2"
+
+# a) Get predictions
+nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
+with st.form("my_form"):
+   st.write("Inside the form")
+
+   input = st.text_input('What is your current routine?')
+
+   # Every form must have a submit button.
+   submitted = st.form_submit_button("Submit")
+   if submitted:
+       QA_input = {
+           'question': input,
+           'context': 'Evaluate this weightlifting routine and make suggestions'
+       }
+       res = nlp(QA_input)
+       st.write(res)
+
+
+
+# # b) Load model & tokenizer
+# model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+# tokenizer = AutoTokenizer.from_pretrained(model_name)
+
